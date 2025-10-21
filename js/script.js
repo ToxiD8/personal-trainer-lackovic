ec18145cfa1e3dfe8081168417ad1566ba94d46b
ToxiD <toxid8@gmail.com>
2025-10-21 21:05:45 +0200

    Intersection Observer - animation runs only once when scrolled to a section

--- js/script.js
+++ js/script.js
@@ -153,8 +153,10 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
+      observer.unobserve(entry.target);
+      // } else {
+      //   entry.target.classList.remove("show");
+      // }
     }
   });
 });
